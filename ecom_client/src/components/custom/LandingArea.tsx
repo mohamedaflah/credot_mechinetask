8d9ace92608b3d66ebc2d3551966fa13f1077ad1
Aflu <mohamedaflah186@gmail.com>
2024-05-01 17:19:07 +0530

    feat: updated landing page productCard

--- ecom_client/src/components/custom/LandingArea.tsx
+++ ecom_client/src/components/custom/LandingArea.tsx
@@ -1,10 +1,20 @@
-
 import LandingImage from "@/assets/LandingPageImage.png";
+import { getCartProductIds } from "@/redux/actions/cart/getCartProductIds";
+import { AppDispatch, RootState } from "@/redux/store";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 // import { FaAngleLeft } from "react-icons/fa6";
 // import { FaAngleRight } from "react-icons/fa6";
 
 function LandingArea() {
+  const { user } = useSelector((state: RootState) => state.user);
+  const dispatch: AppDispatch = useDispatch();
+  useEffect(() => {
+    if (user && user._id) {
+      dispatch(getCartProductIds(String(user?._id)));
+    }
+  }, [dispatch, user]);
   return (
     <div className="w-full">
       <img src={LandingImage} alt="Image" className="object-cover" />
